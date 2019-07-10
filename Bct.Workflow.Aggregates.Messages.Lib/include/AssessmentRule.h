a8f504f67b79274766a36a71f593ade02b7bced0
kclubb <kclubb@gmail.com>
2019-07-10 08:59:48 -0600

    temp to save changes

--- Bct.Workflow.Aggregates.Messages.Lib/include/AssessmentRule.h
+++ Bct.Workflow.Aggregates.Messages.Lib/include/AssessmentRule.h
@@ -9,12 +9,20 @@
 class AssessmentRule
 {
 public:
-   AssessmentRule(std::string ruleId, std::string condition, std::string expression) : _ruleId(ruleId), _condition(condition), _expression(expression) {};
-   std::string RuleId() { return _ruleId; }
+   AssessmentRule(std::string ruleId, std::string stringId, std::string condition, std::string expression) : _ruleId(ruleId), _stringId(stringId), _condition(condition), _expression(expression) {};
+   std::string RuleId()
+   {
+      return _ruleId;
+   }
+   std::string StringId()
+   {
+      return _stringId;
+   }
    std::string Condition() { return _condition; }
    std::string Expression() { return _expression; }
 private:
    std::string _ruleId;
+   std::string _stringId;
    std::string _condition;
    std::string _expression;
 };
