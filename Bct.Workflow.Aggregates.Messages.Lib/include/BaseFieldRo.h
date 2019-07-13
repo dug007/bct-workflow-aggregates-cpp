5e0020e2ed483c46db2e70104b5ee9f403109776
kclubb <kclubb@gmail.com>
2019-07-13 16:28:52 -0600

    checkpoint working

--- Bct.Workflow.Aggregates.Messages.Lib/include/BaseFieldRo.h
+++ Bct.Workflow.Aggregates.Messages.Lib/include/BaseFieldRo.h
@@ -8,14 +8,26 @@ namespace Bct
    {
       namespace Aggregates
       {
+         /// <summary>
+         /// Base template class for read-only scalar fields. All read-only scalar fields must derive from this class.
+         /// </summary>
          template<class T>
          class BaseFieldRo : public BaseField<T>
          {
          private:
             using BaseField<T>::Value; // hide value set/get
          public:
+            /// <summary>
+            /// Constructor.
+            /// </summary>
+            /// <param name="fieldName">Name of this field.</param>
+            /// <param name="t">Type of this field. The given type should be consistent with the template class.</param>
+            /// <param name="metaData">Metadata vector.</param>
+            /// <param name="aggregate">The associated aggregate this field is a member of.</param>
             BaseFieldRo(const std::string fieldName, TypeEnum::Type t, VersionMetaData &metaData, AbstractAggregate *aggregate)
-               : BaseField<T>(fieldName, t, metaData, aggregate) {}
+               : BaseField<T>(fieldName, t, metaData, aggregate)
+            {
+            }
             const T Value()
             {
                return BaseField<T>::Value();
