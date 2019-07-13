c6320ab61f3c5d77629c41d2c7a319de1211989e
kclubb <kclubb@gmail.com>
2019-07-12 18:36:41 -0600

    checkpoint

--- Bct.Workflow.Aggregates.Messages.Lib/include/BaseField.h
+++ Bct.Workflow.Aggregates.Messages.Lib/include/BaseField.h
@@ -38,10 +38,10 @@ namespace Bct
             /// <param name="ver">Version of aggregate this field is associated with.</param>
             /// <param name="metaData">Metadata vector.</param>
             /// <param name="aggregate">The associated aggregate this field is a member of.</param>
-            BaseField(const std::string fieldName, const TypeEnum::Type t, const int16_t ver, VersionMetaData &metaData,  AbstractAggregate *aggregate)
+            BaseField(const std::string fieldName, const TypeEnum::Type t, const int16_t ver, VersionMetaData & metaData,  AbstractAggregate *aggregate)
                : _fieldName(fieldName), _type(t),  _ver(ver), _metaData(metaData), _aggregate(aggregate), _fieldSetCounter(0)
             {
-               // search for the metadata for matching version and field name, then initialize this field
+               // search for the metadata for corresponding version and field name, then initialize this field
                // with that located metadata instance
                FieldMeta fm = findFieldMeta();
                FieldStateEnum::FieldState state = fm._fieldState;
@@ -53,9 +53,9 @@ namespace Bct
             // Set/Get --------------------->
 
             /// <summary>
-            /// 
+            /// Set the value of this field.
             /// </summary>
-            /// <param name="v"></param>
+            /// <param name="v">Value to give this field.</param>
             void Value(const T v)
             {
                if (_state == FieldStateEnum::Constant)
@@ -65,6 +65,10 @@ namespace Bct
                ValueInternal(v, false);
             }
 
+            /// <summary>
+            /// Get the value of this field.
+            /// </summary>
+            /// <returns>The value of this field.</returns>
             T Value() const
             {
                // rules to implement here - User Story 12698
@@ -95,28 +99,48 @@ namespace Bct
 
             // Set/Get ------------------------------<
 
+            /// <summary>
+            /// Conversion operator.
+            /// </summary>
+            /// <returns>The value of this field.</returns>
             operator T() const
             {
                return this->Value();
             }
 
             // AbstractField ------------>
-
+            
+            /// <summary>
+            /// Get the name of this field.
+            /// </summary>
+            /// <returns>Name of this field.</returns>
             virtual  std::string FieldName() const
             {
                return _fieldName;
             }
 
+            /// <summary>
+            /// Get the type of this field.
+            /// </summary>
+            /// <returns>Type of this field.</returns>
             virtual  TypeEnum::Type Type() const
             {
                return _type;
             }
 
+            /// <summary>
+            /// Get the state of this field.
+            /// </summary>
+            /// <returns>State of this field.</returns>
             virtual  FieldStateEnum::FieldState State() const
             {
                return _state;
             }
 
+            /// <summary>
+            /// Get the field set counter for this field. This provides the relative order of when fields were last set.
+            /// </summary>
+            /// <returns>Field set counter.</returns>
             virtual uint32_t FieldSetCounter() const
             {
                return _fieldSetCounter;
@@ -124,12 +148,20 @@ namespace Bct
 
             // AbstractField -------------------<
 
-            const std::string DefaultStr()
+            /// <summary>
+            /// Get the default value of this field as a string.
+            /// </summary>
+            /// <returns>Default value of field as a string.</returns>
+            std::string DefaultStr()
             {
                return findFieldMeta()._default;
             }
 
-            const bool HasValue() const
+            /// <summary>
+            /// Determins if this field has a value.
+            /// </summary>
+            /// <returns>true if the field has a  value, false if the field does not hava a value.</returns>
+            bool HasValue() const
             {
                // TODO handle Computed after field as been computed - User Story 126600
                return (_state == FieldStateEnum::Set || _state == FieldStateEnum::Constant || _state == FieldStateEnum::Default);
@@ -139,6 +171,10 @@ namespace Bct
 
             // AbstractField ----------->
 
+            /// <summary>
+            /// Gets the string representation value of this field. This function is not public and is only needed for RPN computations.
+            /// </summary>
+            /// <returns>String representation of this field.</returns>
             virtual  std::string ComputedValueString() const
             {
                // TODO Use serialization library for string<->type conversion - User Story 126886
@@ -147,6 +183,10 @@ namespace Bct
                return ss.str();
             }
 
+            /// <summary>
+            /// Sets the value of this field using its string representation. This function is not public and is only needed for RPN computations.
+            /// </summary>
+            /// <param name="val">String representation of this field.</param>
             virtual void ComputedValueString(const std::string & val)
             {
                // TODO Use serialization library for string<->type conversion - User Story 126886
@@ -158,6 +198,28 @@ namespace Bct
             }
 
             // AbstractField ---------<
+
+            /// <summary>
+            /// Sets default value.
+            /// </summary>
+            /// <param name="def">Default value.</param>
+            void SetDefault(const T def)
+            {
+               _default = def;
+               _val = def;
+            }
+
+         private:
+
+            T _val;
+            T _default;
+            FieldStateEnum::FieldState _state;
+            TypeEnum::Type _type;
+            std::string _fieldName;
+            VersionMetaData _metaData;
+            int16_t _ver;
+            uint32_t _fieldSetCounter;
+            AbstractAggregate *_aggregate;
 
             void ValueInternal(const T v, bool fromCalculation)
             {
@@ -188,24 +250,6 @@ namespace Bct
                }
 
             }
-
-            void SetDefault(const T def)
-            {
-               _default = def;
-               _val = def;
-            }
-
-         private:
-
-            T _val;
-            T _default;
-            FieldStateEnum::FieldState _state;
-            TypeEnum::Type _type;
-            std::string _fieldName;
-            VersionMetaData _metaData;
-            int16_t _ver;
-            uint32_t _fieldSetCounter;
-            AbstractAggregate *_aggregate;
 
             const FieldMeta findFieldMeta()
             {
