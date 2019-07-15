44731eb07d65c877847cce5726b615e89af19089
kclubb <kclubb@gmail.com>
2019-07-15 07:45:27 -0600

    ckeckpoint working

--- Bct.Workflow.Aggregates.Messages.Lib/src/AssessmentRule.cpp
+++ Bct.Workflow.Aggregates.Messages.Lib/src/AssessmentRule.cpp
@@ -1,26 +1,47 @@
 #include "AssessmentRule.h"
+#include <sstream>
 
-AssessmentRule::AssessmentRule(std::string ruleId, std::string stringId, std::string condition, std::string expression)
-   : _ruleId(ruleId), _stringId(stringId), _condition(condition), _expression(expression)
+namespace Bct
 {
+   namespace Workflow
+   {
+      namespace Aggregates
+      {
+         AssessmentRule::AssessmentRule(std::string ruleId, std::string stringId, std::string condition, std::string expression, std::string versions)
+            : _ruleId(ruleId), _stringId(stringId), _condition(condition), _expression(expression), _versions(versions)
+         {
+         }
+
+         std::string AssessmentRule::RuleId() const
+         {
+            return _ruleId;
+         }
+
+         std::string AssessmentRule::StringId() const
+         {
+            return _stringId;
+         }
+
+         std::string AssessmentRule::Condition() const
+         {
+            return _condition;
+         }
+
+         std::string AssessmentRule::Expression() const
+         {
+            return _expression;
+         }
+
+         bool AssessmentRule::InVersion(int16_t ver)
+         {
+            std::ostringstream ss;
+            ss << '.' << ver << '.';
+            std::string verStr = ss.str();
+            std::size_t f = _versions.find(verStr);
+            return (f != std::string::npos);
+         }
+
+      }
+   }
 }
 
-std::string AssessmentRule::RuleId() const
-{
-   return _ruleId;
-}
-
-std::string AssessmentRule::StringId() const
-{
-   return _stringId;
-}
-
-std::string AssessmentRule::Condition() const
-{
-   return _condition;
-}
-
-std::string AssessmentRule::Expression() const
-{
-   return _expression;
-}
