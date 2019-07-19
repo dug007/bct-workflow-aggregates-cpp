1223697242e5c89882f257048c1805045f21f6d6
Rachelle Walter <rachelle.walter@terumobct.com>
2019-07-19 09:15:37 -0600

    Updated comments to doxygen style

--- Bct.Workflow.Aggregates.Messages.Lib/include/Exceptions.h
+++ Bct.Workflow.Aggregates.Messages.Lib/include/Exceptions.h
@@ -12,33 +12,34 @@ namespace Bct
          class NoSuchVersion : public std::exception
          {
          public:
-            /**
-             * Instantiate a NoSuchVersion exception
-             * @param aggregateName: associated aggregate name for the exception
-             * @param requestedVersion: the version requested that caused the exception
-             *
-             * This exception is thrown if the requested version for an aggregate is not defined
-             */
+            /// <summary>
+            /// Instantiate a NoSuchVersion exception which is thrown if the requested version for an aggregate is not defined.
+            /// </summary>
+            /// <param name="aggregateName">Associated aggregate name for exception</param>
+            /// <param name="requestedVersion">Version requested that caused the exception</param>
             NoSuchVersion(const std::string& aggregateName, const std::string& requestedVersion);
 
-            /**
-             * Destructor 
-             */
+            /// <summary>
+            /// Destructor
+            /// </summary>
             virtual ~NoSuchVersion();
 
-            /**
-             * Return the error message for this exception
-             */
+            /// <summary>
+            /// Error message for the exception
+            /// </summary>
+            /// <returns>The error message for this exception</returns>
             virtual const char * what() const;
 
-            /**
-             * Return the aggregate name associated with this exception
-             */
+            /// <summary>
+            /// Aggregate name for exception
+            /// </summary>
+            /// <returns>Aggregate name associated with this exception</returns>
             const std::string& aggregateName() const;
 
-            /**
-             * Return the requested version associated with this exception
-             */
+            /// <summary>
+            /// Requested version for exception
+            /// </summary>
+            /// <returns>The requested version associated with this exception</returns>
             const std::string& requestedVersion() const;
 
          private:
