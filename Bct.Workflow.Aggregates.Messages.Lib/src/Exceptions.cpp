956db2e1a3d6f69f1902169c2d56214e138c6eb9
Rachelle Walter <rachelle.walter@terumobct.com>
2019-07-19 09:17:20 -0600

    Updated namespace to be wrapped in curly braces rather than stated for every function

--- Bct.Workflow.Aggregates.Messages.Lib/src/Exceptions.cpp
+++ Bct.Workflow.Aggregates.Messages.Lib/src/Exceptions.cpp
@@ -1,38 +1,47 @@
 #include <sstream>
 #include "Exceptions.h"
 
-Bct::Workflow::Aggregates::NoSuchVersion::NoSuchVersion(const std::string& aggregateName, const std::string& requestedVersion)
-   : std::exception()
-   , _errorMessage()
-   , _aggregateName(aggregateName)
-   , _requestedVersion(requestedVersion)
+namespace Bct
 {
-   try
+   namespace Workflow
    {
-      std::ostringstream errorMessageBuilder(_errorMessage);
-      errorMessageBuilder << "Bct::Workflow::Aggregates::NoSuchVersion: aggregate=" << _aggregateName << " requestedVersion=" << _requestedVersion;
+      namespace Aggregates
+      {
+         NoSuchVersion::NoSuchVersion(const std::string& aggregateName, const std::string& requestedVersion)
+            : std::exception()
+            , _errorMessage()
+            , _aggregateName(aggregateName)
+            , _requestedVersion(requestedVersion)
+         {
+            try
+            {
+               std::ostringstream errorMessageBuilder(_errorMessage);
+               errorMessageBuilder << "Bct::Workflow::Aggregates::NoSuchVersion: aggregate=" << _aggregateName << " requestedVersion=" << _requestedVersion;
+            }
+            catch (...)
+            {
+               _errorMessage = "Bct::Workflow::Aggregates::NoSuchVersion";
+            }
+         }
+
+         NoSuchVersion::~NoSuchVersion()
+         {
+         }
+
+         const char * NoSuchVersion::what() const
+         {
+            return _errorMessage.c_str();
+         }
+
+         const std::string& NoSuchVersion::aggregateName() const
+         {
+            return _aggregateName;
+         }
+
+         const std::string& NoSuchVersion::requestedVersion() const
+         {
+            return _requestedVersion;
+         }
+      }
    }
-   catch (...)
-   {
-      _errorMessage = "Bct::Workflow::Aggregates::NoSuchVersion";
-   }
-}
-
-Bct::Workflow::Aggregates::NoSuchVersion::~NoSuchVersion()
-{
-}
-
-const char * Bct::Workflow::Aggregates::NoSuchVersion::what() const
-{
-   return _errorMessage.c_str();
-}
-
-const std::string& Bct::Workflow::Aggregates::NoSuchVersion::aggregateName() const
-{
-   return _aggregateName; 
-}
-
-const std::string& Bct::Workflow::Aggregates::NoSuchVersion::requestedVersion() const
-{
-   return _requestedVersion;
 }
