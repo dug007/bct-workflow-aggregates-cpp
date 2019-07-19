8c873b36d79981d36849adc88c94a9245190fa0b
Rachelle Walter <rachelle.walter@terumobct.com>
2019-07-19 13:55:20 -0600

    Updated Unit tests to reflect new exception being thrown

--- TestBCT.Common.Messages.CPP/test/ComputeFieldUnitTests.cpp
+++ TestBCT.Common.Messages.CPP/test/ComputeFieldUnitTests.cpp
@@ -21,8 +21,8 @@ TEST_CASE("PlateletTemplateAggregate100", "[test]")
    //Test for field that is Unavailable
    CHECK(Platelet_100.minYield.State() == FieldStateEnum::FieldState::Unavailable);
    CHECK(Platelet_100.maxYield.State() == FieldStateEnum::FieldState::Unavailable);
-   CHECK_THROWS_AS(Platelet_100.minYield.Value(), char*);
-   CHECK_THROWS_AS(Platelet_100.maxYield.Value(), char*);
+   CHECK_THROWS_AS(Platelet_100.minYield.Value(), NotAbleToGet);
+   CHECK_THROWS_AS(Platelet_100.maxYield.Value(), NotAbleToGet);
 
    //Check initial state of fields used in calculation
    CHECK(Platelet_100.volumeMl.State() == FieldStateEnum::FieldState::NotSet);
