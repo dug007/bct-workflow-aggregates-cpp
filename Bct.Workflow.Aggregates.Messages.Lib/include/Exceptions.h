0f4593b3cb49bf0fee1d8a5df2411f24b497eba9
Rachelle W <48997727+rwalusb3@users.noreply.github.com>
2019-07-22 10:07:01 -0600

    Added doxygen comments to new exception classes

--- Bct.Workflow.Aggregates.Messages.Lib/include/Exceptions.h
+++ Bct.Workflow.Aggregates.Messages.Lib/include/Exceptions.h
@@ -51,11 +51,41 @@ namespace Bct
          class NotAbleToSet : public std::exception
          {
          public:
+            /// <summary>
+            /// Instantiate a NotAbleToSet exception which is thrown if the requested field is trying to set its value while the state is Constant or Unavailable
+            /// </summary>
+            /// <param name="aggregateName">Associated aggregate name for exception</param>
+            /// <param name="fieldName">Name of the field trying to be set</param>
+            /// <param name="fieldState">State of the field trying to be set</param>
             NotAbleToSet(const std::string& aggregateName, const std::string& fieldName, const std::string& fieldState);
+
+            /// <summary>
+            /// Destructor
+            /// </summary>
             virtual ~NotAbleToSet();
+
+            /// <summary>
+            /// Error message for the exception
+            /// </summary>
+            /// <returns>The error message for this exception</returns>
             virtual const char * what() const;
+
+            /// <summary>
+            /// Aggregate name for exception
+            /// </summary>
+            /// <returns>Aggregate name associated with this exception</returns>
             const std::string& aggregateName() const;
+
+            /// <summary>
+            /// Field name for exception
+            /// </summary>
+            /// <returns>The field name associated with this exception</returns>
             const std::string& fieldName() const;
+
+            /// <summary>
+            /// Field state for exception
+            /// </summary>
+            /// <returns>The state of the field associated with this exception</returns>
             const std::string& fieldState() const;
 
          private:
@@ -68,11 +98,41 @@ namespace Bct
          class NotAbleToGet : public std::exception
          {
          public:
+            /// <summary>
+            /// Instantiate a NotAbleToGet exception which is thrown if the requested field value is trying to be obtained while the field state is Unavailable or NotSet
+            /// </summary>
+            /// <param name="aggregateName">Associated aggregate name for exception</param>
+            /// <param name="fieldName">Name of the field trying to be set</param>
+            /// <param name="fieldState">State of the field trying to be set</param>
             NotAbleToGet(const std::string& aggregateName, const std::string& fieldName, const std::string& fieldState);
+
+            /// <summary>
+            /// Destructor
+            /// </summary>
             virtual ~NotAbleToGet();
+
+            /// <summary>
+            /// Error message for the exception
+            /// </summary>
+            /// <returns>The error message for this exception</returns>
             virtual const char * what() const;
+
+            /// <summary>
+            /// Aggregate name for exception
+            /// </summary>
+            /// <returns>Aggregate name associated with this exception</returns>
             const std::string& aggregateName() const;
+
+            /// <summary>
+            /// Field name for exception
+            /// </summary>
+            /// <returns>The field name associated with this exception</returns>
             const std::string& fieldName() const;
+
+            /// <summary>
+            /// Field state for exception
+            /// </summary>
+            /// <returns>The state of the field associated with this exception</returns>
             const std::string& fieldState() const;
 
          private:
