e3645edb5a69b54bbbb53f9b6815980c77ef4f05
Rachelle W <48997727+rwalusb3@users.noreply.github.com>
2019-08-06 15:55:02 -0600

    Added Configuration, nuspec, and target files for nuget package. Also changed directory path for Lib project to point to RPNEvaluator include folder.

---
+++ Bct.Workflow.Aggregates.Messages.Lib/RPNEvaluatorCPP/include/TokenClass.h
@@ -0,0 +1,12 @@
+#ifndef TokenClass_H
+#define TokenClass_H
+namespace Bct
+{
+	namespace RPNEvaluator
+	{
+      static int variable_t = 0;
+      static int funcOper_t = 1;
+      static int value_t = 2;
+   }
+}
+#endif
