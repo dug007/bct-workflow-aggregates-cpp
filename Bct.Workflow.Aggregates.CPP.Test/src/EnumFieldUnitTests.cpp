139feab6612a772c2a5c3bc777d1e2f50c49148a
kclubb <kclubb@gmail.com>
2019-08-15 06:01:52 -0600

    Added enum unit test

---
+++ Bct.Workflow.Aggregates.CPP.Test/src/EnumFieldUnitTests.cpp
@@ -0,0 +1,14 @@
+#include "EnumField.h"
+#include "EnumFieldRo.h"
+#include "ReferenceAggregate.h"
+
+#include "catch.hpp"
+
+using namespace Bct::Workflow::Aggregates;
+using namespace Bct::Workflow::Implementation;
+
+TEST_CASE("EnumFieldTests", "[test]")
+{
+   ReferenceAggregate ref;
+
+}
