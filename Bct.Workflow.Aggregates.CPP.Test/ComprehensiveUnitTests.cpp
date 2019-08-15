a47b6163ada371ffaf0af6554fe9161bded84cc7
Rachelle W <48997727+rwalusb3@users.noreply.github.com>
2019-08-15 09:38:05 -0600

    Added new unit test file

---
+++ Bct.Workflow.Aggregates.CPP.Test/ComprehensiveUnitTests.cpp
@@ -0,0 +1,13 @@
+#include "ReferenceAggregate.h"
+#include "ReferenceEnum.h"
+#include "EnumField.h"
+#include "catch.hpp"
+
+using namespace Bct::Workflow;
+using namespace Bct::Workflow::Implementation;
+
+TEST_CASE("CheckFieldValues", "[test]")
+{
+   ReferenceAggregate aggregate1;
+
+}
