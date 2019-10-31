d4162535aa53190ce46e0208145d9c0aec9a06d8
kclubb <kclubb@gmail.com>
2019-10-31 12:16:41 -0600

    Updated ReferenceAggregate to latest generated from YAML file

--- Bct.Workflow.Aggregates.CPP.GeneratedCode/include/SimpleAggregate.h
+++ Bct.Workflow.Aggregates.CPP.GeneratedCode/include/SimpleAggregate.h
@@ -10,8 +10,6 @@
 #include "AggComputeField.h"
 #include "StringField.h"
 #include "VectorField.h"
-#include "SimpleAggregate.h"
-#include "SimpleAggregate.h"
 
 using namespace Bct::Workflow::Aggregates;
 
@@ -23,114 +21,44 @@ namespace Bct
       namespace Implementation
       {
 
-         namespace ReferenceEnum
-         {
-            enum Reference
-            {
-               /// <summary>very good reference</summary>
-               VeryGood = 0, 
-               /// <summary>good reference</summary>
-               Good = 1, 
-               /// <summary>average reference</summary>
-               Average = 2, 
-               /// <summary>below average reference</summary>
-               BelowAverage = 4, 
-               /// <summary>poor reference</summary>
-               Poor = 8, 
-               /// <summary>very poor reference</summary>
-               VeryPoor = 16
-            };
-         }
 
          /// <summary>
-         /// ReferenceAggregate.
+         /// Simple Type.
          /// </summary>
-         class ReferenceAggregate : public BaseAggregate
+         class SimpleAggregate : public BaseAggregate
          {
          private:
             void pushFields();
 
          public:
             /// <summary>
-            /// Boolean field
+            /// float field
             /// </summary>
-            BaseField<bool>                     boolField;
-                        /// <summary>
-            /// integer field
-            /// </summary>
-            BaseField<int32_t>                     int32Field;
-                        /// <summary>
-            /// uinteger field
-            /// </summary>
-            BaseField<uint32_t>                     uint32Field;
-                        /// <summary>
-            /// integer field
-            /// </summary>
-            BaseField<int64_t>                     int64Field;
-                        /// <summary>
-            /// uinteger field
-            /// </summary>
-            BaseField<uint64_t>                     uint64Field;
-                        /// <summary>
-            /// double field
-            /// </summary>
-            BaseField<double>                     doubleField;
-                        /// <summary>
-            /// string field
-            /// </summary>
-            StringField                     stringField;
-                        /// <summary>
-            /// 
-            /// </summary>
-            BaseField<bool>                     boolFieldRequiredv2;
-                        /// <summary>
-            /// 
-            /// </summary>
-            EnumField<int32_t, ReferenceEnum::Reference>                     enumField;
-                        /// <summary>
-            /// 
-            /// </summary>
-            VectorField<int32_t>                     vectorIntField;
-                        /// <summary>
-            /// Simple Type.
-            /// </summary>
-            SimpleAggregate                     nestedField;
-                        /// <summary>
-            /// double field
-            /// </summary>
-            BaseField<double>                     v100Field;
-                        /// <summary>
-            /// 
-            /// </summary>
-            SimpleAggregate                     nestedField2;
-                        /// <summary>
-            /// 
-            /// </summary>
-            BaseField<bool>                     boolFieldRequiredv0;
+            BaseField<double>                     doubleValue;
             
             /// <summary>
             /// Default constructor. Creates the most recent version.
             /// </summary>
-            ReferenceAggregate();
+            SimpleAggregate();
 
             /// <summary>
             /// Version constructor. Creates the specified version.
             /// </summary>
             /// <param name="version">Version to construct such as "1.2.0"</param>
-            ReferenceAggregate(const std::string &version);
+            SimpleAggregate(const std::string &version);
 
             /// <summary>
             /// Constructor for nested aggregate.
             /// </summary>
             /// <param name="fieldId">Field id of this nested aggregate.</param>
             /// <param name="parent">Parent aggregate of this nested aggregate</param>
-            ReferenceAggregate(int32_t fieldId, BaseAggregate * const parent);
+            SimpleAggregate(int32_t fieldId, BaseAggregate * const parent);
 
             /// <summary>
             /// Copy constructor.
             /// </summary>
             /// <param name="other">Aggregate to be copied.</param>
-            ReferenceAggregate(const ReferenceAggregate & other);
+            SimpleAggregate(const SimpleAggregate & other);
 
             /// <summary>
             /// Constructor for copying when the aggregate is nested.
@@ -140,7 +68,7 @@ namespace Bct
             /// </summary>
             /// <param name="other">The object being copied</param>
             /// <param name="parent">The parent of this nested aggregate.</param>
-            ReferenceAggregate(const ReferenceAggregate & other, BaseAggregate * const parent);
+            SimpleAggregate(const SimpleAggregate & other, BaseAggregate * const parent);
 
 
             /// <summary>
@@ -148,26 +76,26 @@ namespace Bct
             /// </summary>
             /// <param name="other">Object being copied.</param>
             /// <returns>Reference to new object.</returns>
-            ReferenceAggregate & operator=(const ReferenceAggregate &other);
+            SimpleAggregate & operator=(const SimpleAggregate &other);
 
             /// <summary>
             /// Equal operator.
             /// </summary>
             /// <param name="fld">Other field to compare to</param>
             /// <returns>True if the two field's values are equal.</returns>
-            bool operator ==(const ReferenceAggregate &other) const;
+            bool operator ==(const SimpleAggregate &other) const;
 
             /// <summary>
             /// Not equal operator.
             /// </summary>
             /// <param name="fld">Other field to compare to.</param>
             /// <returns>True if the two field's values are not equal.</returns>
-            bool operator !=(const ReferenceAggregate &other) const;
+            bool operator !=(const SimpleAggregate &other) const;
 
             /// <summary>
             /// Virtual destructor
             /// </summary>
-            virtual ~ReferenceAggregate();
+            virtual ~SimpleAggregate();
 
             /// <summary>
             /// Virtual function to return the metadata reference for this object. Aggregates must implement this at the most-derived level such as generated code.
---
+++ Bct.Workflow.Aggregates.CPP.GeneratedCode/src/SimpleAggregate.cpp
@@ -0,0 +1,113 @@
+#include "SimpleAggregate.h"
+#include "AbstractAggregate.h"
+#include "BaseAggregate.h"
+
+using namespace Bct::Workflow::Aggregates;
+
+
+
+namespace Bct
+{
+   namespace Workflow
+   {
+      namespace Implementation
+      {
+         SimpleAggregate::SimpleAggregate() :
+            SimpleAggregate(BaseAggregate::UseMostRecentVersionStr)
+         {
+         }
+
+         SimpleAggregate::SimpleAggregate(const std::string &version) :
+            BaseAggregate(version),
+            doubleValue(0, this)
+          {
+            pushFields();
+            syncVersion();
+          }
+
+         SimpleAggregate::SimpleAggregate(int32_t fieldId, BaseAggregate * const parent) :
+            BaseAggregate(fieldId, parent),
+           doubleValue(0, this)
+         {
+            pushFields();
+            syncVersion();
+         }
+
+         SimpleAggregate::SimpleAggregate(const SimpleAggregate & other) :
+            BaseAggregate(other),
+            doubleValue(other.doubleValue, this)
+         {
+            pushFields();
+         }
+
+         SimpleAggregate::SimpleAggregate(const SimpleAggregate & other, BaseAggregate * const parent) :
+            BaseAggregate(other, parent),
+            doubleValue(other.doubleValue, this)
+         {
+            pushFields();
+         }
+
+         SimpleAggregate & SimpleAggregate::operator=(const SimpleAggregate &other)
+         {
+            if (&other != this)
+            {
+               BaseAggregate::operator=(other);
+               doubleValue = other.doubleValue;
+            }
+            
+            return *this;
+         }
+
+         bool SimpleAggregate::operator==(const SimpleAggregate &other) const
+         {
+            if (doubleValue != other.doubleValue)
+            {
+               return false;
+            }
+            return true;
+         }
+
+         bool SimpleAggregate::operator!=(const SimpleAggregate &other) const
+         {
+            return ! (*this == other);
+         }
+
+         SimpleAggregate::~SimpleAggregate()
+         {
+         }
+
+         AggregateMetaData & SimpleAggregate::MetaData() const
+         {
+            return s_MetaData();
+         };
+
+         void SimpleAggregate::pushFields()
+         {
+            FieldList().push_back(&doubleValue);
+         }
+
+         AggregateMetaData & SimpleAggregate::s_MetaData()
+         {
+            static AggregateMetaData tm;
+            static bool initialized = false;
+
+            if (!initialized)
+            {
+               tm.addVersion("v1.0.0");
+
+               tm.addField(0, "doubleValue", TypeEnum::DoubleType);
+               tm.addFieldMetaToAllVersions(0, FieldStateEnum::NotSet, "notset");
+
+
+
+               initialized = true;
+            }
+
+            return tm;
+         }
+
+
+      }
+   }
+}
+
