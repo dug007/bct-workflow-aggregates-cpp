786616e26270ec622b04f09c36df529e04dc7803
kclubb <kclubb@gmail.com>
2019-11-25 15:11:02 -0700

    Changed exceptions to be caught by reference

--- Bct.Workflow.Aggregates.CPP.Test/test/QAComprehensiveUnitTests.cpp
+++ Bct.Workflow.Aggregates.CPP.Test/test/QAComprehensiveUnitTests.cpp
@@ -588,7 +588,7 @@ TEST_CASE("ThrowsExceptionIfFieldConstant", "[test]")
    {
       SamAgg4.Field7c.Value(5);
    }
-   catch (NotAbleToSet exc)
+   catch (NotAbleToSet &exc)
    {
       std::string expected = "Bct::Workflow::Aggregates::NotAbleToSet: aggregate=class Bct::Workflow::Implementation::Sample1Aggregate fieldName=Field7c fieldState=Constant";
       std::string actual = exc.what();
@@ -607,7 +607,7 @@ TEST_CASE("ThrowsExceptionIfFieldComputeOnly", "[test]")
    {
       SamAgg5.Field7com.Value(5);
    }
-   catch (NotAbleToSet exc)
+   catch (NotAbleToSet &exc)
    {
       std::string expected = "Bct::Workflow::Aggregates::NotAbleToSet: aggregate=class Bct::Workflow::Implementation::Sample1Aggregate fieldName=Field7com fieldState=Computed";
       std::string actual = exc.what();
@@ -803,7 +803,7 @@ TEST_CASE("ThrowExceptionNoSuchVersion", "[test]")
    {
       Sample1Aggregate SamAgg11("1.0.0.0");
    }
-   catch (NoSuchVersion ex)
+   catch (NoSuchVersion &ex)
    {
       CHECK(ex.requestedVersion() == "1.0.0.0");
       std::string message = "Bct::Workflow::Aggregates::NoSuchVersion: aggregate=class Bct::Workflow::Implementation::Sample1Aggregate requestedVersion=1.0.0.0";
