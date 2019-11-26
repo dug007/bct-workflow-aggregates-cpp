22233397e7e903f40b44e985f4f54f65d81c4282
kclubb <kclubb@gmail.com>
2019-11-26 08:37:58 -0700

    Changed TestHelperBaseField.h to work on vxworks.

--- Bct.Workflow.Aggregates.CPP.Test/test/TestHelperBaseField.h
+++ Bct.Workflow.Aggregates.CPP.Test/test/TestHelperBaseField.h
@@ -9,17 +9,43 @@ namespace Bct
    {
       namespace Aggregates
       {
+         /// <summary>
+         /// Simple helper to allow exposing ComputedValueString for unit testing
+         /// </summary>
          template<class T>
          class TestHelperBaseField : public BaseField<T>
          {
          private:
            
          public:
-            using AbstractField::computedValueString;
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            /// <param name="fieldId">Field id.</param>
+            /// <param name="aggregate">Owner aggregate of this field.</param>
             TestHelperBaseField(int32_t fieldId, AbstractAggregate * const aggregate)
                : BaseField<T>(fieldId, aggregate)
             {
             }
+
+            /// <summary>
+            /// Public exposure of ComputedValueString for testing purposes.
+            /// </summary>
+            /// <param name="val"></param>
+            virtual void ComputedValueString(const std::string & val)
+            {
+               BaseField<T>::ComputedValueString(val);
+            }
+
+            /// <summary>
+            /// Public exposure of ComputedValueString for testing purposes.
+            /// </summary>
+            /// <returns>The string representation of field value.</returns>
+            virtual std::string ComputedValueString() const
+            {
+               return BaseField<T>::ComputedValueString();
+            }
+
 
             virtual ~TestHelperBaseField()
             {
